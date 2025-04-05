b249f40070fee168113dd12927ed80d482ab793d
gyullee96 <gyullee96@naver.com>
2025-04-05 15:45:55 +0900

    second commot

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   }
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${37.6569856}&lon=${127.0546432}&appid=7657ac7041e2a19e52d07d00c8fc3307&units=metric`
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7657ac7041e2a19e52d07d00c8fc3307&units=metric`
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
